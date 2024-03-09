4b0cfea5e499836ac586965dccfad254aeb29689
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-03-09 11:28:01 +0100

    Minor Issues fixed

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -17,10 +17,10 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char	*d;
 	unsigned char	*s;
 
+	if (!dst && !src)
+		return (NULL);
 	d = dst;
 	s = src;
-	if (!dst)
-		return (NULL);
 	while (n > 0)
 	{
 		*d++ = *s++;
