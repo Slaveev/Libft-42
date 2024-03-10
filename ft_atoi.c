3d0b9467ffb0ac637abe74943e9b17bfd27324f4
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-03-10 16:15:34 +0100

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,12 +6,16 @@
 /*   By: dslaveev <dslaveev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 20:52:15 by dslaveev          #+#    #+#             */
-/*   Updated: 2024/03/05 19:02:19 by dslaveev         ###   ########.fr       */
+/*   Updated: 2024/03/09 15:41:31 by dslaveev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/* @brief Converts integer to char
+*  @param char *str
+*  @return returns string of characters
+*/
 int	ft_atoi(const char *str)
 {
 	int	i;
