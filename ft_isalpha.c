e6b9693f6c249d2f8712b0475a2a8725d807857d
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-03-10 16:17:03 +0100

    Update ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,12 +6,15 @@
 /*   By: dslaveev <dslaveev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 20:23:57 by dslaveev          #+#    #+#             */
-/*   Updated: 2024/03/04 20:25:28 by dslaveev         ###   ########.fr       */
+/*   Updated: 2024/03/09 15:47:42 by dslaveev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief check where there is alphabetical character
+/// @param c
+/// @return returns true if c is alphabetical character
 int	ft_isalpha(int c)
 {
 	if (ft_islower(c) || ft_isupper(c))
