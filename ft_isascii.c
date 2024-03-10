bed19fa1bf1927ab3ab8dc85c4e890845980705d
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-03-10 16:17:22 +0100

    Update ft_isascii.c

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,15 +6,18 @@
 /*   By: dslaveev <dslaveev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 20:05:39 by dslaveev          #+#    #+#             */
-/*   Updated: 2024/03/04 20:15:21 by dslaveev         ###   ########.fr       */
+/*   Updated: 2024/03/09 15:48:14 by dslaveev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief check whether there is ascii value
+/// @param c
+/// @return returns true if c is ascii
 int	ft_isascii(int c)
 {
-	if (c >= 0 && c <= 128)
+	if (c >= 0 && c <= 127)
 		return (1);
 	return (0);
 }
