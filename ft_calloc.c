5a801e93075c84a8f7de7abf583e6180e96a6308
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-03-10 16:16:25 +0100

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,24 @@
 /*   By: dslaveev <dslaveev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 19:04:03 by dslaveev          #+#    #+#             */
-/*   Updated: 2024/03/05 19:33:39 by dslaveev         ###   ########.fr       */
+/*   Updated: 2024/03/09 15:45:47 by dslaveev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t num, size_t size)
+/// @brief allocate space for count objects that are size bytes
+/// and fills them with zeros
+/// @param count
+/// @param size
+/// @return returns a pointer to allocated memory
+void	*ft_calloc(size_t count, size_t size)
 {
 	void	*p;
 
-	p = (void *)malloc(num * size);
+	p = (void *)malloc(count * size);
 	if (!p)
 		return (0);
-	ft_bzero(p, num);
+	ft_bzero(p, count * size);
 	return (p);
 }
