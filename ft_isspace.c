0d188de36b2b312276f93706da9369cb5aa65ac0
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-03-10 16:18:19 +0100

    Update ft_isspace.c

--- ft_isspace.c
+++ ft_isspace.c
@@ -6,12 +6,15 @@
 /*   By: dslaveev <dslaveev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 20:19:57 by dslaveev          #+#    #+#             */
-/*   Updated: 2024/03/04 20:20:52 by dslaveev         ###   ########.fr       */
+/*   Updated: 2024/03/09 15:51:19 by dslaveev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief tests for white-spaces
+/// @param c
+/// @return returns zero if the character tests fails
 int	ft_isspace(int c)
 {
 	if (c == 32 || (c >= 9 && c <= 13))
