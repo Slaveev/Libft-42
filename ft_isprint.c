ff9f32fd32765db768aee6c80365bae8cc027871
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-03-10 16:18:00 +0100

    Update ft_isprint.c

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,15 +6,18 @@
 /*   By: dslaveev <dslaveev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 20:18:54 by dslaveev          #+#    #+#             */
-/*   Updated: 2024/03/04 20:19:32 by dslaveev         ###   ########.fr       */
+/*   Updated: 2024/03/09 15:49:17 by dslaveev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief check whether there are only printable characters
+/// @param c
+/// @return returns true if printable characters only
 int	ft_isprint(int c)
 {
-	if (c >= 32 && c <= 127)
+	if (c >= 32 && c <= 126)
 		return (1);
 	return (0);
 }
