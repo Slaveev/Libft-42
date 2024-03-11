37b9eacfe8dbf1322d2c01417a904411ed20b9bb
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-03-11 18:22:48 +0100

    Update ft_strcpy.c

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,12 +6,16 @@
 /*   By: dslaveev <dslaveev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 20:31:39 by dslaveev          #+#    #+#             */
-/*   Updated: 2024/03/04 20:43:42 by dslaveev         ###   ########.fr       */
+/*   Updated: 2024/03/09 16:17:15 by dslaveev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief copies a string source to string destination
+/// @param dest
+/// @param src
+/// @return new destination string
 char	*ft_strcpy(char *dest, char *src)
 {
 	char	*ret;
