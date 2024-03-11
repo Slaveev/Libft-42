4dea89df56580ee37d64533a1d808a42688c20b3
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-03-11 18:19:53 +0100

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,16 +6,21 @@
 /*   By: dslaveev <dslaveev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 11:04:51 by dslaveev          #+#    #+#             */
-/*   Updated: 2024/03/05 11:14:24 by dslaveev         ###   ########.fr       */
+/*   Updated: 2024/03/09 16:06:50 by dslaveev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief copy memory from one place to another
+/// @param dst
+/// @param src
+/// @param n
+/// @return the destination that is coppied
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned char	*d;
-	unsigned char	*s;
+	unsigned char		*d;
+	const unsigned char	*s;
 
 	if (!dst && !src)
 		return (NULL);
