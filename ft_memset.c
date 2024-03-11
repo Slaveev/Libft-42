2b5c7df855dcaf137da1697c58178b91d8d5afee
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-03-11 18:20:43 +0100

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -6,12 +6,17 @@
 /*   By: dslaveev <dslaveev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 10:42:28 by dslaveev          #+#    #+#             */
-/*   Updated: 2024/03/05 10:48:07 by dslaveev         ###   ########.fr       */
+/*   Updated: 2024/03/09 16:11:30 by dslaveev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief sets string with character c len n
+/// @param b
+/// @param c
+/// @param len
+/// @return filled pointer
 void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	*dst;
