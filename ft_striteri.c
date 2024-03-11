293d657f95e8bdac02837fde8b474b8ef171df82
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-03-11 18:24:25 +0100

    Update ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,12 +6,15 @@
 /*   By: dslaveev <dslaveev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 10:22:52 by dslaveev          #+#    #+#             */
-/*   Updated: 2024/03/08 12:09:58 by dslaveev         ###   ########.fr       */
+/*   Updated: 2024/03/09 16:20:43 by dslaveev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief applies function to each character or integer
+/// @param s
+/// @param f
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	size_t	index;
