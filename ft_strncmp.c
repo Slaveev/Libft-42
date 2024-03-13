5c8b47831cb8c9b7e725a6864335d61c7d45bf6b
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-03-13 09:57:15 +0100

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,23 +6,29 @@
 /*   By: dslaveev <dslaveev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 09:27:04 by dslaveev          #+#    #+#             */
-/*   Updated: 2024/03/05 09:33:08 by dslaveev         ###   ########.fr       */
+/*   Updated: 2024/03/09 16:31:32 by dslaveev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(char *dest, const char *src, int n)
+/// @brief compare if n indexes of one string to another
+/// @param dest
+/// @param src
+/// @param n
+/// @return difference bettwen indexes
+int	ft_strncmp(char *dest, const char *src, size_t n)
 {
 	unsigned char	*str1;
 	unsigned char	*str2;
 
 	str1 = (unsigned char *)dest;
 	str2 = (unsigned char *)src;
-	while ((*str1 || *str2) && n-- > 0)
+	while ((*str1 || *str2) && n > 0)
 	{
 		if (*(str1++) != *(str2++))
 			return (*(str1 - 1) - *(str2 - 1));
+		n--;
 	}
 	return (0);
 }
