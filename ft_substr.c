492ad69ec542189ee8afb907348f8c4b23546c08
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-03-13 09:58:34 +0100

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,12 +6,17 @@
 /*   By: dslaveev <dslaveev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 19:53:49 by dslaveev          #+#    #+#             */
-/*   Updated: 2024/03/08 11:35:18 by dslaveev         ###   ########.fr       */
+/*   Updated: 2024/03/09 16:37:10 by dslaveev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief searches for a substring in a string
+/// @param s
+/// @param start
+/// @param len
+/// @return substring that was searched
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
@@ -35,27 +40,3 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	ptr[i] = '\0';
 	return (ptr);
 }
-
-// char	*ft_substr(char const *s, unsigned int start, size_t len)
-// {
-// 	char	*ptr;
-// 	size_t	i;
-
-// 	if (!s)
-// 		return (NULL);
-// 	if (start > len)
-// 		return (ft_strdup(""));
-// 	i = 0;
-// 	if (len >= ft_strlen(s + start))
-// 		len = ft_strlen(s + start);
-// 	ptr = ft_calloc(len + 1, sizeof(char));
-// 	if (!ptr)
-// 		return (NULL);
-// 	while (i < len)
-// 	{
-// 		ptr[i] = s[start + i];
-// 		i++;
-// 	}
-// 	ptr[i] = '\0';
-// 	return (ptr);
-// }
